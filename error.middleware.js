ea2b853c9ddad128536c39d2e61e83307238ea3e
Yehor <antonovegor2003@gmail.com>
2024-04-22 22:03:39 +0300

    CREATE DATABASE_LAB_2

--- error.middleware.js
+++
@@ -1,5 +0,0 @@
-const errorResponder = (err, request, response, next) => {
-    response.header("Content-Type", 'application/json')
-    response.status(err.statusCode).send(err.message)
- }
- module.exports = { errorResponder };
